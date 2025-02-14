c88879f6c843dce10adb3884518746140e47d39a
Alexander Potter <156364135+PotterKEC@users.noreply.github.com>
2025-02-14 16:01:13 +0000

    added streets list

--- src/main/java/PizzaTime.java
+++ src/main/java/PizzaTime.java
@@ -3,6 +3,8 @@ public class PizzaTime {
 
     public static void main(String[] args){
         System.out.println("Pizza Time!");
+
+        string[] streets = new string[]{"main", "portage"};
         
     }
 }
